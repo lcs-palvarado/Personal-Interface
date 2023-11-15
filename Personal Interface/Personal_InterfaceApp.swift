668fc7c500b3b06827ddaf2beef33b1b0ad9e176
Pablo Alvarado <palvarado@lcs.on.ca>
2023-11-15 11:57:23 -0500

    Initial Commit

---
+++ Personal Interface/Personal_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_InterfaceApp.swift
+//  Personal Interface
+//
+//  Created by Pablo Alvarado on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Personal_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
