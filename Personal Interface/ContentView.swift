1049027a2560941e4615f2726274f1032983fd39
Pablo Alvarado <palvarado@lcs.on.ca>
2023-11-16 09:43:54 -0500

    Finished the basics. need to make it work

--- Personal Interface/ContentView.swift
+++ Personal Interface/ContentView.swift
@@ -30,9 +30,25 @@ struct ContentView: View {
             }
             List{
                 Group{
+                  PodcastShow(pictureAsset: "TheRadicalAiPodcast", podcastTitle: "The Radical AI Podcast", podcastAuthor: "")
+                    
+                PodcastShow(pictureAsset: "ReconciliableDifferences", podcastTitle: "Reconciliable Differences", podcastAuthor: "RELAY FM")
+                    
+                    PodcastShow(pictureAsset: "SwiftOverCoffee", podcastTitle: "Swift Over Coffee", podcastAuthor: "PAUL HUDSON, MIKAELA CARON")
+                    
+                  PodcastShow(pictureAsset: "TheTalkShow", podcastTitle: "The Talk Show With John Gruber", podcastAuthor: "DARING FIREBALL / JOHN GRUBER")
+                    
+                    PodcastShow(pictureAsset: "TechWon't", podcastTitle: "Tech Won't Save Us", podcastAuthor: "PARIS MARX")
+                    
+                  PodcastShow(pictureAsset: "UnderTheRadar", podcastTitle: "Under The Radar", podcastAuthor: "RELAY FM")
+                    
+                    PodcastShow(pictureAsset: "WaitingForReview", podcastTitle: "Waiting For Review", podcastAuthor: "NO IDEA")
                     
                 }
+                .listRowSeparatorTint(.gray)
+                .listRowInsets(.none)
             }
+            .listStyle(.plain)
             
             
         }
