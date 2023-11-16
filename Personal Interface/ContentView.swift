e1e09c034ab7ab7c1b5f49dd2ff27641246177fa
Pablo Alvarado <palvarado@lcs.on.ca>
2023-11-15 19:43:13 -0500

    Finished the top part. Now I need to start a content view for the parts of the list.

--- Personal Interface/ContentView.swift
+++ Personal Interface/ContentView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Rectangle()
+                .frame(height: 200)
+                ZStack{
+                    VStack{
+                        HStack{
+                            Image(systemName: "gearshape")
+                                .foregroundColor(.blue)
+                            Image(systemName: "arrow.down.circle")
+                                .foregroundColor(.blue)
+                            Spacer()
+                            Image(systemName: "text.badge.plus")
+                                .foregroundColor(.blue)
+                            Image(systemName: "plus")
+                                .foregroundColor(.blue)
+                        }
+                        Image("SearchBar")
+                                    }
+            }
+            List{
+                Group{
+                    
+                }
+            }
+            
+            
         }
-        .padding()
     }
 }
 
---
+++ Personal Interface/PodcastShow.swift
@@ -0,0 +1,20 @@
+//
+//  PodcastShow.swift
+//  Personal Interface
+//
+//  Created by Pablo Alvarado on 15/11/23.
+//
+
+import SwiftUI
+
+struct PodcastShow: View {
+    var body: some View {
+        HStack{
+            
+        }
+    }
+}
+
+#Preview {
+    PodcastShow()
+}
