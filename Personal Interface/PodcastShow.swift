5613131ed406caa62ce221b68783c205d4481413
Pablo Alvarado <palvarado@lcs.on.ca>
2023-11-15 20:07:16 -0500

    finished content view for each member of the list. can't run it because my phone is not IOS 17.

--- Personal Interface/PodcastShow.swift
+++ Personal Interface/PodcastShow.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct PodcastShow: View {
+    
+    let pictureAsset: String
+    let podcastTitle: String
+    let podcastAuthor: String
+    
+    
     var body: some View {
         HStack{
-            
+            Image(pictureAsset)
+                .cornerRadius(2.0)
+            VStack{
+                Text(podcastTitle)
+                    .foregroundStyle(.white)
+                    .font(.system(size: 20))
+                Text(podcastAuthor)
+                    .foregroundStyle(.gray)
+                    .font(.system(size: 17))
+            }
+            Spacer()
+            Circle()
+                .size(.init(width: 20, height: 20))
+                .foregroundStyle(.blue)
         }
     }
 }
 
 #Preview {
-    PodcastShow()
+
+    PodcastShow(pictureAsset: "searchBar", podcastTitle: "1234", podcastAuthor: "123")
 }
